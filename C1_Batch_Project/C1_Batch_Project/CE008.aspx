c790303b1db47a54b692a7ed013a609365e5092b
sanket <snk.bhimani.jnd@gmail.com>
2016-08-24 12:34:34 +0530

    calc added by bhindi

---
+++ C1_Batch_Project/C1_Batch_Project/CE008.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CE008.aspx.cs" Inherits="C1_Batch_Project.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" Height="16px" OnTextChanged="TextBox1_TextChanged" Width="35px"></asp:TextBox>
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            <asp:ListItem>+</asp:ListItem>
+            <asp:ListItem>-</asp:ListItem>
+            <asp:ListItem>*</asp:ListItem>
+            <asp:ListItem>/</asp:ListItem>
+        </asp:DropDownList>
+        <asp:TextBox ID="TextBox2" runat="server" Height="16px" Width="35px" AutoPostBack="True" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+    
+        &nbsp;&nbsp; =&nbsp;&nbsp;
+        <asp:Label ID="ans" runat="server" Text=""></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ C1_Batch_Project/C1_Batch_Project/CE008.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace C1_Batch_Project
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        double a, b, c;
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            method();
+        }
+        public void method()
+        {
+            try
+            {
+                a = double.Parse(TextBox1.Text.ToString());
+                b = double.Parse(TextBox2.Text.ToString());
+                switch (DropDownList1.SelectedValue.ToCharArray()[0])
+                {
+                    case '+': c = a + b; break;
+                    case '-': c = a - b; break;
+                    case '*': c = a * b; break;
+                    case '/': c = a / b; break;
+                }
+                ans.Text = c.ToString();
+            }
+            catch (Exception e)
+            {
+                ans.Text = "waiting for other data";
+            }
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            method();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            method();
+        }
+    }
+}
